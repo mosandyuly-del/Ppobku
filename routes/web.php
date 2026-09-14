b3c6169b06c40198e279673b0e0f67412c648840
mosandyuly <mosandyuly@gmail.com>
2026-09-14 20:12:31 +0800

    Fix 404 error and handle category pages gracefully

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,41 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 Route::get('/', function () {
+    if (view()->exists('home')) {
+        return view('home');
+    }
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('welcome');
+Route::get('/category/{slug}', function ($slug) {
+    $categoryMap = [
+        'game' => 'Games',
+        'pulsa' => 'Pulsa',
+        'data' => 'Data',
+        'pln-token' => 'PLN',
+        'pln-bill' => 'PLN',
+        'pdam' => 'PDAM',
+    ];
+
+    $categoryName = $categoryMap[$slug] ?? $slug;
+    $products = [];
+
+    if (Schema::hasTable('products')) {
+        $products = DB::table('products')
+            ->where('category', 'LIKE', '%' . $categoryName . '%')
+            ->get();
+    }
+
+    return view('category', [
+        'title' => strtoupper(str_replace('-', ' ', $slug)),
+        'products' => $products
+    ]);
 });
+
+Route::get('/login', function() {
+    return view('welcome');
+})->name('login');
