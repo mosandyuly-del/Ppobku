a7beb71034f6c65a020c52c2be5ff4268bccada4
mosandyuly <mosandyuly@gmail.com>
2026-09-27 04:39:30 +0800

    Update seeder untuk reset admin password

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,15 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call([
-            ProductSeeder::class,
-            AdminSeeder::class,
-        ]);
+        $user = User::firstOrNew(['email' => 'mosandy@admin.com']);
+        $user->name = 'Admin Mosandy';
+        $user->password = Hash::make('password123');
+        $user->save();
     }
 }
